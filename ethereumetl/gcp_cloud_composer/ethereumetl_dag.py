3f4bf7119e210aa0e2de8bff31b16d89c382b349
Evgeny Medvedev <medvedev1088@gmail.com>
2018-07-22 22:47:28 +0700

    Add environment variables

--- ethereumetl/gcp_cloud_composer/ethereumetl_dag.py
+++ ethereumetl/gcp_cloud_composer/ethereumetl_dag.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import os
 from datetime import datetime, timedelta
 
 from airflow import models
@@ -8,7 +9,8 @@ from airflow.operators import bash_operator
 # TODO start_date must be in UTC
 default_dag_args = {
     'depends_on_past': False,
-    'start_date': datetime(2018, 1, 1),
+    'start_date': datetime(2015, 7, 31),
+    'end_date': datetime(2016, 7, 1),
     'email': ['evge.medvedev@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -16,29 +18,27 @@ default_dag_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-# These environment variables must be set
-# TODO: Throw error if they are not set
-environment_variables = {
-    'WEB3_PROVIDER_URI': None,
-    'EXPORT_BUCKET': None
-}
-
 # Define a DAG (directed acyclic graph) of tasks.
 # Any task you create within the context manager is automatically added to the
 # DAG object.
 with models.DAG(
-        'ethereumetl_export_dag',
+        'ethereumetl_export_dag6',
         # Daily at 1am
         schedule_interval='0 1 * * *',
         default_args=default_dag_args) as dag:
     setup_command = \
-        'if [ ! -e ~/miniconda/bin/python3 ]; then wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh -O miniconda.sh >> miniconda_install.log 2>&1 && ' \
-        'bash miniconda.sh -u -b -p ~/miniconda >> miniconda_install.log 2>&1; else echo "Miniconda already installed"; fi && ' \
+        'echo "WEB3_PROVIDER_URI: $WEB3_PROVIDER_URI" && echo "EXPORT_BUCKET: $EXPORT_BUCKET" && ' \
+        'echo "EXPORT_DATE: $EXPORT_DATE" && echo "ETHEREUMETL_REPO_BRANCH: $ETHEREUMETL_REPO_BRANCH" && ' \
+        'find ~ -maxdepth 1 -mmin +10 -type f -name ethereumetl_miniconda_install.lock -delete && ' \
+        'if [ ! -e ~/ethereumetl_miniconda_install.lock ] && [ ! -e ~/miniconda/bin/python3 ]; then touch ~/ethereumetl_miniconda_install.lock && ' \
+        'wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh -O miniconda.sh >> miniconda_install.log && ' \
+        'bash miniconda.sh -u -b -p ~/miniconda >> miniconda_install.log && ' \
+        'rm -f ~/ethereumetl_miniconda_install.lock; else echo "Miniconda already installed"; fi && ' \
         'PYTHON3=~/miniconda/bin/python3 && ' \
         'git clone --branch $ETHEREUMETL_REPO_BRANCH http://github.com/medvedev1088/ethereum-etl && cd ethereum-etl && ' \
         'sudo $PYTHON3 -m pip install -r requirements.txt && ' \
         'BLOCK_RANGE=$($PYTHON3 get_block_range_for_date.py -d $EXPORT_DATE -p $WEB3_PROVIDER_URI) && ' \
-        'BLOCK_RANGE_ARRAY=(${BLOCK_RANGE//,/ }) && START_BLOCK={BLOCK_RANGE_ARRAY[0]} && END_BLOCK=${BLOCK_RANGE_ARRAY[1]} && ' \
+        'BLOCK_RANGE_ARRAY=(${BLOCK_RANGE//,/ }) && START_BLOCK=${BLOCK_RANGE_ARRAY[0]} && END_BLOCK=${BLOCK_RANGE_ARRAY[1]} && ' \
         'export CLOUDSDK_PYTHON=/usr/local/bin/python'
 
     export_blocks_and_transactions_command = \
@@ -56,10 +56,18 @@ with models.DAG(
         '-p $WEB3_PROVIDER_URI --output erc20_transfers.csv && ' \
         'gsutil cp erc20_transfers.csv gs://$EXPORT_BUCKET/erc20_transfers/block_date=$EXPORT_DATE/erc20_transfers.csv '
 
+    output_bucket = os.environ.get('EXPORT_BUCKET')
+    if output_bucket is None:
+        raise ValueError('You must set EXPORT_BUCKET environment variable')
+    web3_provider_uri = os.environ.get('WEB3_PROVIDER_URI', 'https://mainnet.infura.io/')
+    ethereumetl_repo_branch = os.environ.get('ETHEREUMETL_REPO_BRANCH', 'master')
+
     environment = {
         'EXECUTION_DATE': '{{ ds }}',
         'EXPORT_DATE': '{{ macros.ds_add(ds, -1) }}',
-        'ETHEREUMETL_REPO_BRANCH': 'master'
+        'ETHEREUMETL_REPO_BRANCH': ethereumetl_repo_branch,
+        'WEB3_PROVIDER_URI': web3_provider_uri,
+        'EXPORT_BUCKET': output_bucket
     }
 
     export_blocks_and_transactions_operator = bash_operator.BashOperator(

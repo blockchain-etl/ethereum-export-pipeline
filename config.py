21dd091cd814b0768a75907a9960346b7ee0ae02
Evgeny Medvedev <medvedev1088@gmail.com>
2018-07-24 14:40:47 +0700

    Change --ipc-path to -p for provider uri

--- config.py
+++ config.py
@@ -24,36 +24,36 @@ EXPORT_ERC20_TOKENS = True
 IS_GETH = False
 
 if IS_GETH:
-    IPC_PATH = '/home/ec2-user/.ethereum/geth.ipc'
+    IPC_PATH = 'file:///home/ec2-user/.ethereum/geth.ipc'
 else:
-    IPC_PATH = '/home/ec2-user/.local/share/io.parity.ethereum/jsonrpc.ipc'
+    IPC_PATH = 'file:///home/ec2-user/.local/share/io.parity.ethereum/jsonrpc.ipc'
 
 SETUP_COMMAND = \
     "cd /home/ec2-user/ethereum-etl && IPC_PATH={} && ".format(IPC_PATH) + \
     "PADDED_START=`printf \"%08d\" $1` && PADDED_END=`printf \"%08d\" $2`"
 
 EXPORT_BLOCKS_AND_TRANSACTIONS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 export_blocks_and_transactions.py -s $1 -e $2 --ipc-path $IPC_PATH -w 1 " + \
+    "python3 export_blocks_and_transactions.py -s $1 -e $2 -p $IPC_PATH -w 1 " + \
     "--blocks-output ${OUTPUT1_STAGING_DIR}/blocks_${PADDED_START}_${PADDED_END}.csv --transactions-output ${OUTPUT2_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv"
 
 EXPORT_RECEIPTS_AND_LOGS_COMMAND = SETUP_COMMAND + ' && ' + \
     "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/tx_hashes.csv.temp -c tx_hash && " + \
-    "python3 export_receipts_and_logs.py --tx-hashes ${OUTPUT1_STAGING_DIR}/tx_hashes.csv.temp --ipc-path $IPC_PATH -w 1 " + \
+    "python3 export_receipts_and_logs.py --tx-hashes ${OUTPUT1_STAGING_DIR}/tx_hashes.csv.temp -p $IPC_PATH -w 1 " + \
     "--receipts-output ${OUTPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv --logs-output ${OUTPUT2_STAGING_DIR}/logs_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/tx_hashes.csv.temp"
 
 EXPORT_CONTRACTS_COMMAND = SETUP_COMMAND + ' && ' + \
     "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -c receipt_contract_address && " + \
-    "python3 export_contracts.py --contract-addresses ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp --ipc-path $IPC_PATH -w 1 " + \
+    "python3 export_contracts.py --contract-addresses ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -p $IPC_PATH -w 1 " + \
     "--output ${OUTPUT1_STAGING_DIR}/contracts_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp"
 
 EXPORT_ERC20_TRANSFERS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 export_erc20_transfers.py -s $1 -e $2 --ipc-path $IPC_PATH -w 1 " + \
+    "python3 export_erc20_transfers.py -s $1 -e $2 -p $IPC_PATH -w 1 " + \
     "--output ${OUTPUT1_STAGING_DIR}/erc20_transfers_${PADDED_START}_${PADDED_END}.csv"
 
 EXPORT_ERC20_TOKENS_COMMAND = SETUP_COMMAND + ' && ' + \
     "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/erc20_transfers_${PADDED_START}_${PADDED_END}.csv -c erc20_token -o - | sort | uniq > ${OUTPUT1_STAGING_DIR}/erc20_token_addresses.csv.temp && " + \
-    "python3 export_erc20_tokens.py --token-addresses ${OUTPUT1_STAGING_DIR}/erc20_token_addresses.csv.temp --ipc-path $IPC_PATH -w 5 " + \
+    "python3 export_erc20_tokens.py --token-addresses ${OUTPUT1_STAGING_DIR}/erc20_token_addresses.csv.temp -p $IPC_PATH -w 5 " + \
     "--output ${OUTPUT1_STAGING_DIR}/erc20_tokens_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/erc20_token_addresses.csv.temp"

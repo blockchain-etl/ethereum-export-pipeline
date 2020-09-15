2dced9eedfd3ef6886820bfb2e6c93d4dec3b9db
Szőke Péter <szokeptr@gmail.com>
2020-09-15 12:40:58 +0200

    Update export commands for latest ethereum-etl version

--- config.py
+++ config.py
@@ -33,27 +33,27 @@ SETUP_COMMAND = \
     "PADDED_START=`printf \"%08d\" $1` && PADDED_END=`printf \"%08d\" $2`"
 
 EXPORT_BLOCKS_AND_TRANSACTIONS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 export_blocks_and_transactions.py -s $1 -e $2 -p $IPC_PATH -w 1 " + \
+    "python3 ethereumetl.py export_blocks_and_transactions -s $1 -e $2 -p $IPC_PATH -w 1 " + \
     "--blocks-output ${OUTPUT1_STAGING_DIR}/blocks_${PADDED_START}_${PADDED_END}.csv --transactions-output ${OUTPUT2_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv"
 
 EXPORT_RECEIPTS_AND_LOGS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -c hash && " + \
-    "python3 export_receipts_and_logs.py --transaction-hashes ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -p $IPC_PATH -w 1 " + \
+    "python3 ethereumetl.py extract_csv_column -i ${INPUT1_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -c hash && " + \
+    "python3 ethereumetl.py export_receipts_and_logs --transaction-hashes ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -p $IPC_PATH -w 1 " + \
     "--receipts-output ${OUTPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv --logs-output ${OUTPUT2_STAGING_DIR}/logs_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp"
 
 EXPORT_CONTRACTS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -c contract_address && " + \
-    "python3 export_contracts.py --contract-addresses ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -p $IPC_PATH -w 1 " + \
+    "python3 ethereumetl.py extract_csv_column -i ${INPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -c contract_address && " + \
+    "python3 ethereumetl.py export_contracts --contract-addresses ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -p $IPC_PATH -w 1 " + \
     "--output ${OUTPUT1_STAGING_DIR}/contracts_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp"
 
 EXPORT_TOKEN_TRANSFERS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 export_token_transfers.py -s $1 -e $2 -p $IPC_PATH -w 1 " + \
+    "python3 ethereumetl.py export_token_transfers -s $1 -e $2 -p $IPC_PATH -w 1 " + \
     "--output ${OUTPUT1_STAGING_DIR}/token_transfers_${PADDED_START}_${PADDED_END}.csv"
 
 EXPORT_TOKENS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 extract_csv_column.py -i ${INPUT1_STAGING_DIR}/token_transfers_${PADDED_START}_${PADDED_END}.csv -c token_address -o - | sort | uniq > ${OUTPUT1_STAGING_DIR}/token_addresses.csv.temp && " + \
-    "python3 export_tokens.py --token-addresses ${OUTPUT1_STAGING_DIR}/token_addresses.csv.temp -p $IPC_PATH -w 5 " + \
+    "python3 ethereumetl.py extract_csv_column -i ${INPUT1_STAGING_DIR}/token_transfers_${PADDED_START}_${PADDED_END}.csv -c token_address -o - | sort | uniq > ${OUTPUT1_STAGING_DIR}/token_addresses.csv.temp && " + \
+    "python3 ethereumetl.py export_tokens --token-addresses ${OUTPUT1_STAGING_DIR}/token_addresses.csv.temp -p $IPC_PATH -w 5 " + \
     "--output ${OUTPUT1_STAGING_DIR}/tokens_${PADDED_START}_${PADDED_END}.csv && " + \
     "rm -f ${OUTPUT1_STAGING_DIR}/token_addresses.csv.temp"
